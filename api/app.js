ee11646e5013eda13e8c1a233dc86ff71ed0be84
Chandini Mahalakshmi <104729699+ychandiniml@users.noreply.github.com>
2024-04-30 09:35:45 +0530

    added user, auth route and created react project in client folder

---
+++ api/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import authRoute from "./routes/authRoute.js";
+import postRoute from "./routes/postRoute.js";
+import testRoute from "./routes/testRoute.js";
+import userRoute from "./routes/userRoute.js";
+import chatRoute from "./routes/chatRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+
+app.listen(8800, () => {
+  console.log("Server is running!");
+});
